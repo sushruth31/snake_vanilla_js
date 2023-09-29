60fbf92877422712bac709bd5adb366c45d0f76a
Sushruth Chandrasekar <sushruth31@gmail.com>
2023-09-28 23:22:29 -0400

    cleanup and more styling

---
+++ utils.js
@@ -0,0 +1,13 @@
+// direction fns
+export const moveRight = ([row, col]) => [row, col + 1];
+export const moveLeft = ([row, col]) => [row, col - 1];
+export const moveUp = ([row, col]) => [row - 1, col];
+export const moveDown = ([row, col]) => [row + 1, col];
+
+export function toId(...args) {
+  return args.join("-");
+}
+
+export function fromId(id) {
+  return id.split("-").map((i) => parseInt(i));
+}
